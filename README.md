8a286beb78d43b9fdc53c368c81f75cedfe1b4ec
Viktor Somodi <somodi.viktor@gmail.com>
2015-07-24 07:52:32 +0200

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# chai-public-equal
+chai-public-equal 
+===========
+
+"publicEqual" object properties matcher for [Chai](http://chaijs.com/) assertion library
+
+Installation
+===========
+
+`npm install --save-dev chai-public-equal`
+
+Usage
+=====
+
+common.js
+```js
+var chai = require('chai');
+var chaiPublicEqual = require('chai-public-equal');
+chai.use(chaiPublicEqual);
+```
+
+in your spec.js
+```js
+var obj = {
+	a: 'b',
+	c: 'd',
+	_d: 'private_value'
+};
+	
+expect(obj).to.eqlPublic({
+	a: 'b',
+	c: 'd'
+});
+//or with 'not'
+expect(obj).to.not.eqlPublic({
+	h: 'b'
+});
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var compare = require('./lib/compare');
+
+module.exports = function(chai, utils) {
+  var Assertion = chai.Assertion;
+  var assertionPrototype = Assertion.prototype;
+
+  Assertion.addChainableMethod('eqlPublic', function (expected) {
+    var actual = utils.flag(this, 'object');
+    var showDiff = chai.config.showDiff;
+
+    assertionPrototype.assert.call(this,
+      compare(expected, actual),
+      'expected #{act} to contain public property values #{exp}',
+      'expected #{act} to not contain public property values #{exp}',
+      expected,
+      actual,
+      showDiff
+    );
+  });
+};
+
+
