628993abac1150050c939d308f3221c3d9a03ee3
Viktor Somodi <somodi.viktor@gmail.com>
2015-07-31 07:20:02 +0200

    add tests for classes

---
+++ test/classes.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class InterSector {
+
+  constructor(a, b) {
+    this._a = a;
+    this._b = b;
+  }
+
+  get intersection() {
+    return this._a - this._b;
+  }
+
+  a() { return this._a; }
+
+}
+
+class OtherInterSector extends InterSector {
+
+  constructor(a, b) {
+    super(a, b);
+  }
+
+  a() { return this._b; }
+
+}
+
+describe('Chai Public Equal', function() {
+
+  describe('with classes', function() {
+
+    it('should equal by their properties', function() {
+      this.expect(new InterSector(2, 1)).to.publicEql(new InterSector(3, 2));
+    });
+
+    it('should ignore functions', function() {
+      this.expect(new InterSector(2, 1)).to.publicEql(new OtherInterSector(3, 2));
+    });
+
+  });
+
+});
---
+++ test/objects.js
@@ -0,0 +1,63 @@
+'use strict';
+
+describe('Chai Public Equal', function() {
+
+  describe('With objects', function() {
+
+    it('should equal by their properties', function() {
+      var actual = { a: 1 };
+      var expected = { a: 1 };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should compare nested objects by value not reference', function() {
+      var actual = { a: 1, nested: { aa: 2 } };
+      var expected = { a: 1, nested: { aa: 2 } };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should ignore private properties', function() {
+      var actual = { a: 1, _b: 2 };
+      var expected = { a: 1, _b: 3 };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should ignore functions', function() {
+      var actual = { a: 1, c: function() { return 1; } };
+      var expected = { a: 1, c: function() { return 2; } };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should ignore functions on nested object', function() {
+      var actual = { a: 1, b: { d: 1, c: function() { return 1; }  } };
+      var expected = { a: 1, b: { d: 1, c: function() { return 2; } } };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should ignore private properties on nested object', function() {
+      var actual = { a: 1, nested: { aa: 2, _bb: 1 } };
+      var expected = { a: 1, nested: { aa: 2, _bb: 2 } };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should ignore properties from the given patterns', function() {
+      var actual = { a: 1, should: 5, b: { c: 1, should: 3, e: 5 } };
+      var expected = { a: 1, should: 5, b: { c: 1, should: 4, e: 5 }  };
+      this.expect(actual).to.publicEql(expected);
+    });
+
+    it('should work with chai negate', function() {
+      var actual = { a: 1, _b: 2 };
+      var expected = { a: 2, _b: 2 };
+      this.expect(actual).to.not.publicEql(expected);
+    });
+
+    it('should work with chai negate with nested object', function() {
+      var actual = { a: 1, b: { c: 1 }, _b: 2 };
+      var expected = { a: 1, b: { c: 2 }, _b: 2 };
+      this.expect(actual).to.not.publicEql(expected);
+    });
+
+  });
+
+});
