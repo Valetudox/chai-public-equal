62631c731a622a7c83325b60ca5472703eee49b7
Viktor Somodi <somodi.viktor@gmail.com>
2015-09-19 07:53:23 +0200

    add object transformer as strategy param

--- asserts/contain-publics.js
+++ asserts/contain-publics.js
@@ -1,10 +1,14 @@
 'use strict';
 
 let _ = require('lodash');
+let rejectKeys = require('../lib/reject-keys');
 
 module.exports = {
   compare: function(expected, actual) {
     return _.matches(expected)(actual);
+  },
+  transform: function(patterns, obj) {
+	return rejectKeys(obj, patterns);
   },
   expectationMessage: 'expected #{act} to contain public properties #{exp}',
   expectationNotMessage: 'expected #{act} to not contain public properties #{exp}'
--- asserts/public-eql.js
+++ asserts/public-eql.js
@@ -1,10 +1,14 @@
 'use strict';
 
 let equal = require('deep-equal');
+let rejectKeys = require('../lib/reject-keys');
 
 module.exports = {
   compare: function(expected, actual) {
     return equal(expected, actual);
+  },
+  transform: function(patterns, obj) {
+	return rejectKeys(obj, patterns);
   },
   expectationMessage: 'expected #{act} to have same public properties #{exp}',
   expectationNotMessage: 'expected #{act} to not have same public properties #{exp}'
