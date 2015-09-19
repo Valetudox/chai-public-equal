2f3bf226fe90f118af5fd313b66d630bd2aaa8fb
Viktor Somodi <somodi.viktor@gmail.com>
2015-09-19 07:31:47 +0200

    use es6 syntax

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var Plugin = require('./lib/plugin');
-var publicEqlAssert = require('./asserts/public-eql');
-var containPublicsAssert = require('./asserts/contain-publics');
+let Plugin = require('./lib/plugin');
+let publicEqlAssert = require('./asserts/public-eql');
+let containPublicsAssert = require('./asserts/contain-publics');
 
 module.exports = function(patterns) {
   return function(chai, utils) {
-    var Assertion = chai.Assertion;
-    var publicEqlAssertPlugin = new Plugin(chai, utils, patterns, publicEqlAssert);
-    var containPublicsAssertPlugin = new Plugin(chai, utils, patterns, containPublicsAssert);
+    let Assertion = chai.Assertion;
+    let publicEqlAssertPlugin = new Plugin(chai, utils, patterns, publicEqlAssert);
+    let containPublicsAssertPlugin = new Plugin(chai, utils, patterns, containPublicsAssert);
 
     Assertion.addChainableMethod('publicEql', function(expected) {
       publicEqlAssertPlugin.test(expected, this);
